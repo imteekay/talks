a75f0714b7e0a3c38579c6baa6e891b0d047eb7a
TK <leandrotk100@gmail.com>
2019-06-25 22:55:33 -0300

    Add some source codes for the fp talk

---
+++ functional_programming/source_code/pure_function.js
@@ -0,0 +1,12 @@
+const calculateArea = (radius, pi) =>
+  radius * radius * pi;
+
+// ------------------------------
+
+const counter = 1;
+
+const increaseCounter = () => counter + 1;
+
+counter; // 1
+increaseCounter(); // 2
+counter; // 1
---
+++ functional_programming/source_code/slugify.js
@@ -0,0 +1,10 @@
+const string = " I will be a url slug   ";
+
+const slugify = string =>
+  string
+    .toLowerCase()
+    .trim()
+    .split(" ")
+    .join("-");
+
+slugify(string); // i-will-be-a-url-slug
