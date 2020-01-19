f8cb41bffb2b4bbf1b50c7c033a1a7d3acf91509
TK <leandrotk100@gmail.com>
2020-01-19 15:32:09 -0300

    fix some files

--- functional_programming/source_code_before/currying.js
+++ functional_programming/source_code_before/currying.js
@@ -2,10 +2,10 @@ let value;
 
 const add = arg1 => arg2 => arg1 + arg2;
 
-const increment = add(1); // => (arg2) => 1 + arg2
+const increment = (num) => num + 1;
 value = increment(2); // 3
 console.log(value);
 
-const add60 = add(60);
+const add60 = (num) => num + 60;
 value = add60(100); // 160
 console.log(value);
