a024d153be30af29d9199f258af81036150f8564
TK <leandrotk100@gmail.com>
2020-10-28 22:30:22 -0300

    More constraints and type nawworing

--- a_mental_model_to_think_in_typescript/007.constrains-and-type-narrowing.ts
+++ a_mental_model_to_think_in_typescript/007.constrains-and-type-narrowing.ts
@@ -9,6 +9,7 @@ half({ username: 'tk' }); // NaN
 half([42, 3.14]); // NaN
 half((a, b) => a + b); // NaN
 
+// Narrowing the input type
 const halfInTS = (x: number) => x / 2;
 
 halfInTS(null); // Argument of type 'null' is not assignable to parameter of type 'number'.
@@ -19,3 +20,67 @@ halfInTS('TK'); // Argument of type '"TK"' is not assignable to parameter of typ
 halfInTS({ username: 'tk' }); // Argument of type '{ username: string; }' is not assignable to parameter of type 'number'.
 halfInTS([42, 3.14]); // Argument of type 'number[]' is not assignable to parameter of type 'number'.
 halfInTS((a, b) => a + b); // Argument of type '(a: any, b: any) => any' is not assignable to parameter of type 'number'.
+
+// Narrowing using typeof
+type StringOrNumber = string | number;
+
+function stringOrNumber(value: StringOrNumber) {
+  if (typeof value === 'string') {
+    // value.
+    // your ide will show you the possible methods from the string type
+    // (parameter) value: string
+    value
+  }
+
+  if (typeof value === 'number') {
+    // value.
+    // your ide will show you the possible methods from the number type
+    // (parameter) value: number
+    value
+  }
+}
+
+// ternary or if conditions as type narrowing
+type User = {
+  name: string;
+  address: {
+    street: string;
+    complement?: string;
+  }
+};
+
+function getComplementLengthV1(user: User): number {
+  return user.address.complement.length;
+  // (property) complement?: string | undefined
+  // Object is possibly 'undefined'.
+}
+
+function getComplementLengthV2(user: User) {
+  return user.address.complement?.length;
+}
+
+function getComplementLengthV3(user: User): number {
+  return user.address.complement
+    ? user.address.complement.length
+    : 0;
+}
+
+function getComplementLengthV4(user: User): number {
+  if (!user.address.complement) return 0;
+  return user.address.complement.length;
+}
+
+getComplementLengthV4({
+  name: 'TK',
+  address: {
+    street: 'Shinjuku Avenue'
+  }
+}); // 0
+
+getComplementLengthV4({
+  name: 'TK',
+  address: {
+    street: 'Shinjuku Avenue',
+    complement: 'A complement'
+  }
+}); // 12
